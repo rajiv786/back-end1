1cc28f9e7be48153ce690626648a51482769a07c
Rajiv Khanduja <49587262+rajiv786@users.noreply.github.com>
2023-05-03 17:02:03 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,35 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const PDFDocument = require('pdfkit');
-const { htmlToText } = require('html-to-text');
-const cors = require('cors');
+const { jsPDF } = require('jspdf');
 const app = express();
-app.use(bodyParser.json());
+const cors = require('cors');
+const base64Img = require('base64-img');
 app.use(cors({}));
-app.post('/api/generate-pdf', async (req, res) => {
-  const { options } = req.body;
 
-  const doc = new PDFDocument(options);
+app.get('/api/generate-pdf', (req, res) => {
+  // Load the PNG image as a base64-encoded string
+  const backgroundImage = base64Img.base64Sync('./check1.png');
+
+  // Create a new PDF document
+  const doc = new jsPDF();
+
+  // Add the PNG image as a background image
+  doc.addImage(backgroundImage, 'PNG', 0, 0, doc.internal.pageSize.width, doc.internal.pageSize.height, undefined, 'FAST');
+   // Set the opacity of the image
+
+  // Add the certificate text
+  doc.setFontSize(30);
+  doc.setTextColor('#000000'); // Set the text color to white
+  doc.text('Certificate of Completion', doc.internal.pageSize.width / 2, 100, { align: 'center' });
+  doc.setFontSize(16);
+  doc.text('This is to certify that Raji1 successfully completed the course Writing Skills by Kevin Missal from Hubhawks MasterClass', doc.internal.pageSize.width / 2, 150, { align: 'center' });
+  doc.setFontSize(12);
+  doc.text('Given on: [DATE]', 50, 250, { align: 'left' });
+  doc.text('Given on: [DATE]', doc.internal.pageSize.width / 2, 250, { align: 'center' });
+  doc.text('[SIGNATURE]', doc.internal.pageSize.width - 50, 250, { align: 'right' });
+
+  // Get the PDF bytes as an ArrayBuffer
+  const pdfBytes = doc.output('arraybuffer');
+
   // Set the response headers
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename="output.pdf"');
-  // Pipe the PDF to the response
-  doc.pipe(res);
-  // Write text to PDF
-  doc.fontSize(40).text('Certificate of Completion', { align: 'center' });
-  doc.moveDown();
-  doc.fontSize(24).text('This is to certify that _Raji1 successfully completed the course _writing skills by Kevin Missal_  from Hubhawks MasterClass', { align: 'center' });
-  doc.moveDown(2);
+  res.setHeader('Content-Disposition', 'attachment; filename=certificate.pdf');
+  res.setHeader('Content-Length', pdfBytes.byteLength);
 
-  doc.moveDown(2);
-  doc.fontSize(16).text('Given on: [DATE]', { align: 'left' });
-  doc.fontSize(16).text('Given on: [DATE]', { align: 'center' });
-  doc.fontSize(16).text('[SIGNATUE]', { align: 'right' });
-  // End the PDF stream
-  doc.end();
+  // Convert the ArrayBuffer to a Buffer and send it as the response
+  res.send(Buffer.from(pdfBytes));
 });
 
-// Start the server
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000, () => {
   console.log('Server listening on port 5000');
 });
